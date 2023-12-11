db884c22b1184424579db492762de70aa12199e7
Kiszel Daniel <74417423+Kdaniel-prog@users.noreply.github.com>
2023-12-11 03:56:47 +0100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserStorageService } from './services/user-storage.service';
+import { APIService } from './services/api.service';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbModule, NavbarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'webshop-frontend';
-}
+  constructor(private userStorageService: UserStorageService, private apiService: APIService) {}
+
+  ngOnInit(): void{
+    this.apiService.getUserIdFromUsername('test').subscribe( 
+      (response) => {
+        this.userStorageService.saveUserid(response.userId);
+      },
+      (error) => {
+        console.error('Error: ', error);
+      }
+    )
+
+    this.apiService.getCart(this.userStorageService.getUserId()).subscribe(
+      (response) => {
+        this.userStorageService.saveCartid(response.id);
+      },
+      (error) => {
+        console.error('Error: ', error);
+      }
+    )
+
+  }
+  refreshProducts(){
+    console.log('hello world')
+  }
+
+ }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserManagerComponent } from './user-manager/user-manager.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { SummaryComponent } from './summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'prefix', component: HomePageComponent},
+    { path: 'user-managment', component: UserManagerComponent},
+    { path: 'shopping-cart', component: ShoppingCartComponent},
+    { path: 'summary', component: SummaryComponent}
+];
